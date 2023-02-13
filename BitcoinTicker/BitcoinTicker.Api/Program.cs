3352e6935846cba718f0113ce76a7aad1392bd9b
agawdat <agawdat@vlenseg.com>
2023-02-13 18:56:03 +0200

    Add docker with SQL server

--- BitcoinTicker/BitcoinTicker.Api/Program.cs
+++ BitcoinTicker/BitcoinTicker.Api/Program.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using BitcoinTicker.Application.BitcoinTicks;
 using BitcoinTicker.Application.Shared.BitcoinTicks;
 using BitcoinTicker.Application.Shared.BitcoinTicks.Dto;
+using BitcoinTicker.EntityFrameworkCore.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 
 builder.Services.AddScoped<IBitcoinPriceFactory, BitcoinPriceFactory>()
                 .AddScoped<IBitcoinPriceBitstampProvider, BitstampBitcoinPriceProvider>()
@@ -22,11 +31,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
